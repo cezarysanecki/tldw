e83cc3c549e291439cbc43aaf07cedee0cfcffc4
Cezary Sanecki <saneckicezary@gmail.com>
2025-04-09 13:33:02 +0200

    fix

--- tldr.py
+++ tldr.py
@@ -5,10 +5,11 @@ from article_summarizer import ArticleContentSummarizer
 def summarize_article(url):
     article_content_extractor = ArticleContentExtractor(url)
     article_metadata = article_content_extractor.run()
+    title = article_metadata['title']
+    content = article_metadata['content']
 
-    article_content_summarizer = ArticleContentSummarizer
-    article_content_summarizer.summarize(article_metadata['title'], article_metadata['content'])
-
+    article_content_summarizer = ArticleContentSummarizer()
+    article_content_summarizer.summarize(title, content)
     title = article_content_summarizer['title']
     paragraph = article_content_summarizer['paragraph']
     paragraph_pl = article_content_summarizer['paragraph_pl']
